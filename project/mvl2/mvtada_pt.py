f2351b909ca49e38bac8fa8fd453f14d4ee1e050
Lobo <austin.talbot1993@gmail.com>
2023-08-06 21:17:00 -0400

    Finished pytorch implementation

--- project/mvl2/mvtada_pt.py
+++ project/mvl2/mvtada_pt.py
@@ -3,24 +3,115 @@ import numpy.random as rand
 import torch
 from tqdm import trange
 from torch import nn
-from torch.distributions import Dirichlet,
+from torch.distributions import Dirichlet
+from copy import deepcopy
 
 
 class MVTadaPT(object):
-
     def __init__(self, K=4, training_options={}):
         self.K = int(K)
         self.training_options = self._fill_training_options(training_options)
 
-    def fit(self,data):
+    def fit(self, data, progress_bar=True):
+        """
 
-        p = data.shape[1]
-        Lamb_init = rand.randn(K,p)
-        lambda_log = torch.tensor(Lamb_init,requires_grad=True)
-        pi_ = torch.tensor(pi_init,requires_grad=True)
+        Parameters
+        ----------
 
-        m_d = Dirichlet(torch.tensor(np.ones(self.K)/self.K))
-        m_p = Dirichlet(torch.tensor(np.ones(self.K)/self.K))
+        Returns
+        -------
 
-    
+        """
+        td = self.training_options
+        K = self.K
 
+        N, p = data.shape
+        X = torch.tensor(data)
+
+        # Initialize variables
+        Lamb_init = rand.randn(K, p).astype(np.float32)
+        pi_init = rand.randn(K).astype(np.float32)
+        lambda_latent = torch.tensor(Lamb_init, requires_grad=True)
+        pi_logits = torch.tensor(pi_init, requires_grad=True)
+
+        trainable_variables = [lambda_latent, pi_logits]
+
+        m_d = Dirichlet(torch.tensor(np.ones(self.K) / self.K))
+        m_d = Dirichlet(torch.tensor(np.ones(self.K) / self.K))
+
+        optimizer = torch.optim.SGD(
+            trainable_variables, lr=td["learning_rate"], momentum=0.9
+        )
+
+        mse = nn.MSELoss()
+        smax = nn.Softmax()
+        softplus = nn.Softplus()
+
+        myrange = trange if progress_bar else range
+        print(type(X))
+        print(X.dtype)
+        print(lambda_latent.dtype)
+        print(pi_logits.dtype)
+        #print(type(lambda_latent))
+        #print(type(pi_logits))
+
+        for i in myrange(td["n_iterations"]):
+            idx = rand.choice(N, td["batch_size"], replace=False)
+            X_batch = X[idx]  # Batch size x
+
+            Lambda_ = softplus(lambda_latent)
+            pi_ = smax(pi_logits)
+
+            loss_logits = 0.001 * mse(
+                pi_logits, torch.zeros(K)
+            )  # Don't allow explosions
+            loss_prior_pi = -1.0 * m_d.log_prob(pi_) / N
+
+            # Log likelihood gamma x^{alpha-1}e^{-beta x}
+            term1 = 4.0 * torch.log(Lambda_)
+            term2 = -5.0 * Lambda_
+            loss_prior_lambda = 1 / N * torch.sum(term1 + term2)
+
+            likelihood_each = [
+                X_batch * torch.log(Lambda_[k]) - Lambda_[k] for k in range(K)
+            ]
+            [torch.sum(mat, axis=1) for mat in likelihood_each]
+            likelihood_mean = [torch.mean(mat) for mat in likelihood_each]
+            like_vec = torch.stack(likelihood_mean)
+            like_components = like_vec + torch.log(pi_)
+            loss_likelihood = -1 * torch.sum(like_components)
+
+            loss = (
+                loss_logits
+                + loss_prior_pi
+                + loss_prior_lambda
+                + loss_likelihood
+            )
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        self.Lambda = Lambda_.detach().numpy()
+        self.pi = pi_.detach().numpy()
+
+    def _fill_training_options(self, training_options):
+        """
+        This fills any relevant parameters for the learning algorithm
+
+        Parameters
+        ----------
+        training_options : dict
+
+        Returns
+        -------
+        training_opts : dict
+        """
+        default_options = {
+            "n_iterations": 5000,
+            "batch_size": 100,
+            "learning_rate": 1e-3,
+        }
+        tops = deepcopy(default_options)
+        tops.update(training_options)
+        return tops
---
+++ project/mvl2/test_pt.py
@@ -0,0 +1,17 @@
+import numpy as np
+import numpy.random as rand
+from mvtada_pt import MVTadaPT
+
+N = 10000
+p = 10
+K = 4
+
+Z_true = rand.randint(K,size=N)
+Lambda = rand.randint(50,size=(K,p))
+X = np.zeros((N,p))
+for i in range(N):
+    X[i] = rand.poisson(Lambda[int(Z_true[i])])
+
+
+model = MVTadaPT()
+model.fit(X.astype(np.float32))
