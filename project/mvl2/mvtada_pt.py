a115663dffcdfdfe8ccb901f3002e9940d45b19e
Lobo <austin.talbot1993@gmail.com>
2023-08-07 15:31:32 -0400

    Fixed logsumexp

--- project/mvl2/mvtada_pt.py
+++ project/mvl2/mvtada_pt.py
@@ -13,7 +13,7 @@ class MVTadaPT(object):
         self.K = int(K)
         self.training_options = self._fill_training_options(training_options)
 
-    def fit(self, data, progress_bar=True):
+    def fit(self, data, progress_bar=True,Lamb_init=None,pi_init=None):
         """
 
         Parameters
@@ -30,15 +30,16 @@ class MVTadaPT(object):
         X = torch.tensor(data)
 
         # Initialize variables
-        Lamb_init = rand.randn(K, p).astype(np.float32)
-        pi_init = rand.randn(K).astype(np.float32)
+        if Lamb_init is None:
+            Lamb_init = 10*rand.randn(K, p).astype(np.float32)
+        if pi_init is None:
+            pi_init = rand.randn(K).astype(np.float32)
         lambda_latent = torch.tensor(Lamb_init, requires_grad=True)
         pi_logits = torch.tensor(pi_init, requires_grad=True)
 
         trainable_variables = [lambda_latent, pi_logits]
 
-        m_d = Dirichlet(torch.tensor(np.ones(self.K) / self.K))
-        m_d = Dirichlet(torch.tensor(np.ones(self.K) / self.K))
+        m_d = Dirichlet(torch.tensor(np.ones(self.K) * self.K))
 
         optimizer = torch.optim.SGD(
             trainable_variables, lr=td["learning_rate"], momentum=0.9
@@ -50,31 +51,35 @@ class MVTadaPT(object):
 
         myrange = trange if progress_bar else range
 
+        self.losses_likelihoods = np.zeros(td['n_iterations'])
+
         for i in myrange(td["n_iterations"]):
             idx = rand.choice(N, td["batch_size"], replace=False)
             X_batch = X[idx]  # Batch size x
 
             Lambda_ = softplus(lambda_latent)
-            pi_ = smax(pi_logits)
+            mu = .5
+            pi_ = smax(pi_logits)*mu + (1-mu)/K
 
             loss_logits = 0.001 * mse(
                 pi_logits, torch.zeros(K)
             )  # Don't allow explosions
-            loss_prior_pi = -1.0 * m_d.log_prob(pi_) / N
+            loss_prior_pi = -1.0 * m_d.log_prob(pi_) / N 
 
             # Log likelihood gamma x^{alpha-1}e^{-beta x}
             term1 = 4.0 * torch.log(Lambda_)
             term2 = -5.0 * Lambda_
             loss_prior_lambda = 1 / N * torch.sum(term1 + term2)
 
-            likelihood_each = [
+            loglikelihood_each = [
                 X_batch * torch.log(Lambda_[k]) - Lambda_[k] for k in range(K)
             ]
-            [torch.sum(mat, axis=1) for mat in likelihood_each]
-            likelihood_mean = [torch.mean(mat) for mat in likelihood_each]
-            like_vec = torch.stack(likelihood_mean)
-            like_components = like_vec + torch.log(pi_)
-            loss_likelihood = -1 * torch.sum(like_components)
+            loglikelihood_sum = [torch.sum(mat, axis=1) for mat in loglikelihood_each]
+            loglikelihood_mean = [torch.mean(mat) for mat in loglikelihood_sum]
+            loglike_vec = torch.stack(loglikelihood_mean)
+            loglike_components = loglike_vec + torch.log(pi_)
+            loss_likelihood = -1*torch.logsumexp(loglike_components,0)
+            #loss_likelihood = -1 * torch.sum(like_components)
 
             loss = (
                 loss_logits
@@ -82,6 +87,12 @@ class MVTadaPT(object):
                 + loss_prior_lambda
                 + loss_likelihood
             )
+            if i % 500 == 0:
+                print(i,loss_likelihood,loss_prior_pi,loss_prior_lambda)
+                #print(pi_)
+                #print(pi_logits)
+            #self.losses_likelihoods
+
 
             optimizer.zero_grad()
             loss.backward()
@@ -142,9 +153,9 @@ class MVTadaPT(object):
         tops : dict
         """
         default_options = {
-            "n_iterations": 5000,
-            "batch_size": 100,
-            "learning_rate": 1e-3,
+            "n_iterations": 100000,
+            "batch_size": 200,
+            "learning_rate": 1e-4,
         }
         tops = deepcopy(default_options)
         tops.update(training_options)
--- project/mvl2/test_pt.py
+++ project/mvl2/test_pt.py
@@ -2,9 +2,11 @@ import numpy as np
 import numpy.random as rand
 from mvtada_pt import MVTadaPT
 
+rand.seed(2021)
+
 N = 10000
-p = 10
-K = 4
+p = 8
+K = 3
 
 Z_true = rand.randint(K,size=N)
 Lambda = rand.randint(50,size=(K,p))
@@ -13,6 +15,19 @@ for i in range(N):
     X[i] = rand.poisson(Lambda[int(Z_true[i])])
 
 
-model = MVTadaPT()
-model.fit(X.astype(np.float32))
+model = MVTadaPT(K=K)
+model.fit(X.astype(np.float32),
+            progress_bar=False)
+            #Lamb_init=Lambda.astype(np.float32),
+            #pi_init=np.zeros(K).astype(np.float32))
 Z_hat = model.predict(X)
+
+
+print(np.mean(X,axis=0))
+print('Estimated clusters')
+print(model.pi)
+
+print('Estimated')
+print(model.Lambda)
+print('True')
+print(Lambda)
